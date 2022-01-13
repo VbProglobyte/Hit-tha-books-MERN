68a06434d66e93145b72ef55126a97d9a4cb347f
Val Brisendine <vsbrisendine@gmail.com>
2022-01-13 13:19:33 -0600

    adding the files and started refactoring

---
+++ Develop/server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+// require the apollo server
+const { ApolloServer } = require('apollo-server-express');
+const db = require('./config/connection');
+const routes = require('./routes');
+// require the schema 
+const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// appolo server variable label "server"  includes args {typeDefs, resolvers}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// middleware express server
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+app.use(routes);
+// open on local host with graphql
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
